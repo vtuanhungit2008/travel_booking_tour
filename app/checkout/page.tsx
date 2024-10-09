7089fde267b280921771753f6c1a43ff2359c19e
Vo Tuan Hung <vtuanhungit2008@gmail.com>
2024-10-09 20:57:44 +0700

    update project from organization

---
+++ app/checkout/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function page() {
+  return (
+    <div>
+      aa
+    </div>
+  )
+}
+
+export default page
---
+++ utils/format.ts
@@ -0,0 +1,21 @@
+export const formatCurrency = (amount: number | null) => {
+    const value = amount || 0;
+    return new Intl.NumberFormat('en-US', {
+      style: 'currency',
+      currency: 'USD',
+      minimumFractionDigits: 0,
+      maximumFractionDigits: 0,
+    }).format(value);
+  };
+
+  export function formatQuantity(quantity: number, noun: string): string {
+    return quantity === 1 ? `${quantity} ${noun}` : `${quantity} ${noun}s`;
+  }
+
+  export const formatDate = (date: Date) => {
+    return new Intl.DateTimeFormat("en-US", {
+      year: "numeric",
+      month: "long",
+      day: "numeric",
+    }).format(date);
+  };
